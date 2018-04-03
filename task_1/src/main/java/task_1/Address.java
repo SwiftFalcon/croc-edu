b7b3563edafb9d697a88066976f12be63136d9c9
SwiftFalcon <korolyov.k@gmail.com>
2018-04-03 23:50:25 +0300

    First Сommit

---
+++ task_1/src/main/java/task_1/Address.java
@@ -0,0 +1,13 @@
+package task_1;
+
+public class Address {
+    public String street;
+    public String zipcode;
+    public String city;
+    
+    public Address(String street, String zipcode, String city) {
+        this.street = street;
+        this.zipcode = zipcode;
+        this.city = city;
+    }
+}
---
+++ task_1/src/main/java/task_1/User.java
@@ -0,0 +1,10 @@
+package task_1;
+
+public class User extends Address{
+
+    public User(String street, String zipcode, String city) {
+        super(street, zipcode, city);
+    }
+    
+    
+}
