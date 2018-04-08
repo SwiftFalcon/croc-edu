f887f5948f94e71b5b716eed3a18fd7232fd1a31
SwiftFalcon <korolyov.k@gmail.com>
2018-04-08 14:04:34 +0300

    Relationship between Bid vs Item and User is completed

--- task_1/src/main/java/task_1/Bid.java
+++ task_1/src/main/java/task_1/Bid.java
@@ -1,5 +1,36 @@
 package task_1;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
+
+//Бинарная ассоциация с User и Item + композиция с Item
+
 public class Bid {
+    private BigDecimal amount;
+    private Date created;
+    private User bidUser;
+    
+    public BigDecimal getAmount() {
+        return amount;
+    }
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+        
+    }
+    public Date getCreated() {
+        return created;
+    }
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+    
+    public User getBidUser() {
+        return bidUser;
+    }
+    public void setBidUser(User bidUser) {
+        this.bidUser = bidUser;
+    }
+    
     
 }
--- task_1/src/main/java/task_1/Item.java
+++ task_1/src/main/java/task_1/Item.java
@@ -1,5 +1,76 @@
 package task_1;
 
-public class Item {
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
+//Бинарная ассоциация и композиция с Bid + ассоциация с Shipment
+
+public class Item {
+    private String name;
+    private String description;
+    private BigDecimal initialPrice;
+    private BigDecimal reversePrice;
+    private Date startDate;
+    private Date endDate;
+    //private ItemState state;
+    public Date approvalDateTime;
+    private Bid successful;
+    private Set<Bid> itemBid = new HashSet<Bid>();
+    
+    public void setItemBid(Bid b){
+        itemBid.add(b);
+    }
+    public Set<Bid> getItemBid(){
+        return itemBid;
+    }
+    public void deleteItemBid(Bid b){
+        itemBid.remove(b);
+    }
+   
+    public Bid getSuccessful() {
+        return successful;
+    }
+    public void setSuccessful(Bid successful) {
+        this.successful = successful;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public BigDecimal getInitialPrice() {
+        return initialPrice;
+    }
+    public void setInitialPrice(BigDecimal initialPrice) {
+        this.initialPrice = initialPrice;
+    }
+    public BigDecimal getReversePrice() {
+        return reversePrice;
+    }
+    public void setReversePrice(BigDecimal reversePrice) {
+        this.reversePrice = reversePrice;
+    }
+    public Date getStartDate() {
+        return startDate;
+    }
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+    public Date getEndDate() {
+        return endDate;
+    }
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+    
+    
 }
